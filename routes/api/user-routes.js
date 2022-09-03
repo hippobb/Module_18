a8d6f9ed2170b53f6707f83781a91a8437d4e0a2
hippobb <88931044+hippobb@users.noreply.github.com>
2022-09-03 15:07:02 -0400

    Update user-routes.js

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -9,22 +9,5 @@ const {
   deleteFriend
 } = require('../../controllers/user-controller');
 
-// /api/users
-router
-  .route('/')
-  .get(getAllUser)
-  .post(createUser);
-
-// /api/users/:id
-router
-  .route('/:id')
-  .get(getUserById)
-  .put(updateUser)
-  .delete(deleteUser);
-
-  router
-  .route('/:userId/friends/:friendId')
-  .post(addFriend)
-  .delete(deleteFriend);
 
 module.exports = router;
