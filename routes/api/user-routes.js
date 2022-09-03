24418ca1b7a03af698c2a3afd0a555f6f5ea4183
hippobb <88931044+hippobb@users.noreply.github.com>
2022-09-03 15:09:01 -0400

    Update user-routes.js

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,13 +1,4 @@
 const router = require('express').Router();
-const {
-  getAllUser,
-  getUserById,
-  createUser,
-  updateUser,
-  deleteUser,
-  addFriend,
-  deleteFriend
-} = require('../../controllers/user-controller');
 
 
 module.exports = router;
